df0bde24c07952eb5271c769a03f58438d3210b6
Jack Wilkin <wilkin.j@husky.neu.edu>
2018-11-30 18:23:35 -0500

    Updates

---
+++ precache-manifest.40e797fc5ebe14888203c73652088e8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3042fff4139f1e9baaedcf4199df2b72",
+    "url": "/MafiaParty2.0/static/media/villager.3042fff4.svg"
+  },
+  {
+    "revision": "16c2b1c8c59cdacf4baf96bca1fc6526",
+    "url": "/MafiaParty2.0/static/media/townhall.16c2b1c8.svg"
+  },
+  {
+    "revision": "84202713fe76871823632e321982a3d2",
+    "url": "/MafiaParty2.0/static/media/mafia.84202713.svg"
+  },
+  {
+    "revision": "d373bc7967d2bc11c93fac8c670fe2e9",
+    "url": "/MafiaParty2.0/static/media/eyesclosed.d373bc79.svg"
+  },
+  {
+    "revision": "3c9fcbf5adf11c5cd27e",
+    "url": "/MafiaParty2.0/static/js/runtime~main.3c9fcbf5.js"
+  },
+  {
+    "revision": "2aab29e9494277a0dc93",
+    "url": "/MafiaParty2.0/static/js/main.2aab29e9.chunk.js"
+  },
+  {
+    "revision": "e471b344dfe0db4dfb7d",
+    "url": "/MafiaParty2.0/static/js/1.e471b344.chunk.js"
+  },
+  {
+    "revision": "2aab29e9494277a0dc93",
+    "url": "/MafiaParty2.0/static/css/main.413604d0.chunk.css"
+  },
+  {
+    "revision": "f7bec04e76b3ba09db346ac08fa46aee",
+    "url": "/MafiaParty2.0/index.html"
+  }
+];
